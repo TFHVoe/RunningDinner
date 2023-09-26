b6ef4b126921dbfd132bb4bd5b2a27ff8201d2a2
TFHVoe <510735@student.fontys.nl>
2023-09-26 11:14:32 +0200

    bezig

--- TeunTest.py
+++ TeunTest.py
@@ -3,29 +3,36 @@ import pandas as pd
 df = pd.read_excel('Running Dinner eerste oplossing 2022.xlsx')
 df_tafelgenoot_2021 = pd.read_excel("Running Dinner dataset 2022.xlsx",sheet_name="Tafelgenoot vorig jaar")
 
-#tv = []
-#for j in range(3,6):
-#    for i in range(len(df)):
-#        x = df.iloc[df[df.iloc[:,3] == df.iloc[0,3]].index.values,1].tolist()
-#        for k in x:
-#            if df.iloc[i, 2] != df.iloc[i, j] != x[i]:
-           
-#                tv.append((df.iloc[i,1],k))
-#print(len(tv))
-#print(tv)
-
-
-
-
-x = df.iloc[df[df.iloc[:,3] == df.iloc[0,3]].index.values,1].tolist()
-print(x)
-
-
-
-
-#print(tv[0])
 #lst = []
-#for i in range(10):
-#    for j in range(10):
-#        lst.append((i,j))
-#print(lst)
+#for i in range(1,len(df_tafelgenoot_2021)):
+#    lst.append((df_tafelgenoot_2021.iloc[i,0], df_tafelgenoot_2021.iloc[i,1]))
+
+lst_voor = []
+#print(df)
+
+for j in range(len(df)):
+    x = df.iloc[:,1][df['Voor'] == df.iloc[j,3]]
+    for i in x:
+        lst_voor.append((df.iloc[j,1],i))
+#print(lst_voor)
+
+
+for i in lst_voor:
+    j = list(i)
+    if j[0] == j[1]:
+        lst_voor.remove(i)
+
+
+con = []
+for i in lst_voor:
+    j = list(i)
+    if j[0] + j[1] and j[1] + j[0] not in con:
+       con.append(j[0]+j[1])
+    else:
+        lst_voor.remove(i)
+print(con)
+print(lst_voor)
+   
+
+     
+
