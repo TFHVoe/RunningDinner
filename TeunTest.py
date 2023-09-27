da85d58b632049e6f01aa7ded8a22b0e67b92afb
TFHVoe <510735@student.fontys.nl>
2023-09-27 09:26:18 +0200

    Buren eis

--- TeunTest.py
+++ TeunTest.py
@@ -1,15 +1,14 @@
 import pandas as pd
 
 df = pd.read_excel('Running Dinner eerste oplossing 2022.xlsx')
-df_tafelgenoot_2021 = pd.read_excel("Running Dinner dataset 2022.xlsx",sheet_name="Tafelgenoot vorig jaar")
+df_buren = pd.read_excel("Running Dinner dataset 2022.xlsx",sheet_name="Buren" )
 
-
-def zelfdetafelpartners2021(df, df_tafelgenoot_2021):
-    """Functie die telt hoe vaak twee deelnemers die in 2022 bij elkaar aan tafel zaten, in 2023 weer elkaars tafelgenoot waren."""
-    countzelfdetafelpartner2021 = 0
+def metdeburenaantafel(df, df_buren):
+    """Functie die telt hoe vaak er met de directe buren aan tafel gezeten wordt"""
+    countzelfdeburen = 0
     lst = []
-    for i in range(1,len(df_tafelgenoot_2021)):
-        lst.append((df_tafelgenoot_2021.iloc[i,0], df_tafelgenoot_2021.iloc[i,1])) #Lijst met alle paren die bij elkaar aan tafel hebben gezeten in 2021.
+    for i in range(1,len(df_buren)):
+        lst.append((df_buren.iloc[i,0], df_buren.iloc[i,1])) #Lijst met alle buren.
 
     lst_2023 = []
     for k in range(3,6):
@@ -21,22 +20,20 @@ def zelfdetafelpartners2021(df, df_tafelgenoot_2021):
        for i in lst_2023:
            j = list(i)
            if j[0] == j[1]:   # Verwijdert het personen paar wanneer de personen in het personenpaar hetzelfde zijn.
-               lst_2023.remove(i)
+                   lst_2023.remove(i)
 
     con = []
     lst_2023_2 = []
     for i in lst_2023:
-           if i[0] + i[1] and i[1] + i[0] not in con: #Controleert of het personen paar niet twee keer voor komt in geval dat a = b en b = a.
-              con.append(i[0]+i[1])
-              lst_2023_2.append(i)
+       if i[0] + i[1] and i[1] + i[0] not in con: #Controleert of het personen paar niet twee keer voor komt in geval dat a = b en b = a.
+          con.append(i[0]+i[1])
+          lst_2023_2.append(i)
 
     for i in lst_2023_2:
-        for j in lst:
-            if i[0]+i[1] == j[0]+j[1] or i[1]+i[0] == j[0]+j[1] or i[0]+i[1] == j[1]+j[0] or i[1]+i[0] == j[1]+j[0]: #Telt 1 op bij de teller wanneer twee personen vorig jaar en dit jaar bij elkaar aan tafel zaten. 
-                countzelfdetafelpartner2021 += 1
-    return countzelfdetafelpartner2021
-       
-print(zelfdetafelpartners2021(df, df_tafelgenoot_2021))
-
+            for j in lst:
+                if i[0]+i[1] == j[0]+j[1] or i[1]+i[0] == j[0]+j[1] or i[0]+i[1] == j[1]+j[0] or i[1]+i[0] == j[1]+j[0]: #Telt 1 op wanneer er met de directe buren aan tafel gezeten wordt.
+                    countzelfdeburen += 1
+    return countzelfdeburen
+print(metdeburenaantafel(df, df_buren))
      
 
--- poging3.py
+++ poging3.py
@@ -50,21 +50,53 @@ def zelfdetafelpartners2021(df, df_tafelgenoot_2021):#Functie die telt hoevaak t
     con = []
     lst_2023_2 = []
     for i in lst_2023:
-           if i[0] + i[1] and i[1] + i[0] not in con: #Controleert of het personen paar niet twee keer voor komt in geval dat a = b en b = a.
-              con.append(i[0]+i[1])
-              lst_2023_2.append(i)
+        if i[0] + i[1] and i[1] + i[0] not in con: #Controleert of het personen paar niet twee keer voor komt in geval dat a = b en b = a.
+            con.append(i[0]+i[1])
+            lst_2023_2.append(i)
 
     for i in lst_2023_2:
         for j in lst:
             if i[0]+i[1] == j[0]+j[1] or i[1]+i[0] == j[0]+j[1] or i[0]+i[1] == j[1]+j[0] or i[1]+i[0] == j[1]+j[0]: #Telt 1 op bij de teller wanneer twee personen vorig jaar en dit jaar bij elkaar aan tafel zaten. 
                 countzelfdetafelpartner2021 += 1
     return countzelfdetafelpartner2021
+def metdeburenaantafel(df, df_buren):#Functie die telt hoevaak er met de driecte buren aan tafel gezeten wordt.
+    """Functie die telt hoe vaak er met de directe buren aan tafel gezeten wordt"""
+    countzelfdeburen = 0
+    lst = []
+    for i in range(1,len(df_buren)):
+        lst.append((df_buren.iloc[i,0], df_buren.iloc[i,1])) #Lijst met alle buren.
+
+    lst_2023 = []
+    for k in range(3,6):
+       for j in range(len(df)):
+           x = df.iloc[:,1][df.iloc[:,k] == df.iloc[j,k]] #Lijst van alle paren die bij elkaar gaan zitten per gang.
+           for i in x:
+                lst_2023.append((df.iloc[j,1],i))
+
+       for i in lst_2023:
+           j = list(i)
+           if j[0] == j[1]:   # Verwijdert het personen paar wanneer de personen in het personenpaar hetzelfde zijn.
+                   lst_2023.remove(i)
+
+    con = []
+    lst_2023_2 = []
+    for i in lst_2023:
+       if i[0] + i[1] and i[1] + i[0] not in con: #Controleert of het personen paar niet twee keer voor komt in geval dat a = b en b = a.
+          con.append(i[0]+i[1])
+          lst_2023_2.append(i)
+
+    for i in lst_2023_2:
+            for j in lst:
+                if i[0]+i[1] == j[0]+j[1] or i[1]+i[0] == j[0]+j[1] or i[0]+i[1] == j[1]+j[0] or i[1]+i[0] == j[1]+j[0]: #Telt 1 op wanneer er met de directe buren aan tafel gezeten wordt.
+                    countzelfdeburen += 1
+    return countzelfdeburen
 
 #Data inladen 
 df = pd.read_excel('Running Dinner eerste oplossing 2022.xlsx')
 df_adressen = pd.read_excel("Running Dinner dataset 2022.xlsx",sheet_name="Adressen" )
 df_kookte_2021 = pd.read_excel("Running Dinner dataset 2022.xlsx",sheet_name="Kookte vorig jaar")
 df_tafelgenoot_2021 = pd.read_excel("Running Dinner dataset 2022.xlsx",sheet_name="Tafelgenoot vorig jaar")
+df_buren = pd.read_excel("Running Dinner dataset 2022.xlsx",sheet_name="Buren" )
 
 #Dictionarys met de beslisvariable 
 gt = {} #Gangtoewijzing A --> G
@@ -85,4 +117,6 @@ print(count_hoofdgerecht2022)
 count_voorkeur = voorkeursgang(df, df_adressen)
 print(count_voorkeur)
 count_zelfde_tafelpartner_2021 = zelfdetafelpartners2021(df, df_tafelgenoot_2021)
-print(count_zelfde_tafelpartner_2021) 
+print(count_zelfde_tafelpartner_2021)
+count_met_buren_aan_tafel = metdeburenaantafel(df, df_buren) 
+print(count_met_buren_aan_tafel)
