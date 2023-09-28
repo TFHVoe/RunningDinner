6b79d6a857cdb22dae7705e8e2a5a992a99ae8a2
TFHVoe <510735@student.fontys.nl>
2023-09-28 15:56:43 +0200

    laatste wens

--- poging3.py
+++ poging3.py
@@ -65,12 +65,12 @@ def voorkeursgang(df, df_adressen):#Functie die telt hoevaak een voorkeur gang j
             if j == l and k == m: #Twee for loops die kijken voor ieder huisaderes of hier de voorkeur gang gekookt wordt.
                 count_voorkeur += 1
     return count_voorkeur
-def zelfdetafelpartners2021(df, df_tafelgenoot_2021):#Functie die telt hoevaak twee tafelgenoten in 2022 ook de tafelgenoot van 2023 waren.
+def zelfdetafelpartners2022(df, df_tafelgenoot_2022):#Functie die telt hoevaak twee tafelgenoten in 2022 ook de tafelgenoot van 2023 waren.
     """Functie die telt hoe vaak twee deelnemers die in 2022 bij elkaar aan tafel zaten, in 2023 weer elkaars tafelgenoot waren."""
     countzelfdetafelpartner2021 = 0
     lst = []
-    for i in range(1,len(df_tafelgenoot_2021)):
-        lst.append((df_tafelgenoot_2021.iloc[i,0], df_tafelgenoot_2021.iloc[i,1])) #Lijst met alle paren die bij elkaar aan tafel hebben gezeten in 2021.
+    for i in range(1,len(df_tafelgenoot_2022)):
+        lst.append((df_tafelgenoot_2022.iloc[i,0], df_tafelgenoot_2022.iloc[i,1])) #Lijst met alle paren die bij elkaar aan tafel hebben gezeten in 2022.
 
     lst_2023 = []
     for k in range(3,6):
@@ -126,6 +126,36 @@ def metdeburenaantafel(df, df_buren):#Functie die telt hoevaak er met de driecte
                 if i[0]+i[1] == j[0]+j[1] or i[1]+i[0] == j[0]+j[1] or i[0]+i[1] == j[1]+j[0] or i[1]+i[0] == j[1]+j[0]: #Telt 1 op wanneer er met de directe buren aan tafel gezeten wordt.
                     countzelfdeburen += 1
     return countzelfdeburen
+def zelfdetafelpartners2021(df, df_tafelgenoot_2021):#Functie die telt hoevaak twee tafelgenoten in 2021 ook de tafelgenoot van 2023 waren.
+    """Functie die telt hoe vaak twee deelnemers die in 2022 bij elkaar aan tafel zaten, in 2023 weer elkaars tafelgenoot waren."""
+    countzelfdetafelpartner2021 = 0
+    lst = []
+    for i in range(1,len(df_tafelgenoot_2021)):
+        lst.append((df_tafelgenoot_2021.iloc[i,0], df_tafelgenoot_2021.iloc[i,1])) #Lijst met alle paren die bij elkaar aan tafel hebben gezeten in 2021.
+
+    lst_2023 = []
+    for k in range(3,6):
+       for j in range(len(df)):
+           x = df.iloc[:,1][df.iloc[:,k] == df.iloc[j,k]] #Lijst van alle paren die bij elkaar gaan zitten per gang.
+           for i in x:
+                lst_2023.append((df.iloc[j,1],i))
+
+       for i in lst_2023:
+           if i[0] == i[1]:   # Verwijdert het personen paar wanneer de personen in het personenpaar hetzelfde zijn.
+               lst_2023.remove(i)
+
+    con = []
+    lst_2023_2 = []
+    for i in lst_2023:
+        if i[0] + i[1] and i[1] + i[0] not in con: #Controleert of het personen paar niet twee keer voor komt in geval dat a = b en b = a.
+            con.append(i[0]+i[1])
+            lst_2023_2.append(i)
+
+    for i in lst_2023_2:
+        for j in lst:
+            if i[0]+i[1] == j[0]+j[1] or i[1]+i[0] == j[0]+j[1] or i[0]+i[1] == j[1]+j[0] or i[1]+i[0] == j[1]+j[0]: #Telt 1 op bij de teller wanneer twee personen vorig jaar en dit jaar bij elkaar aan tafel zaten. 
+                countzelfdetafelpartner2021 += 1
+    return countzelfdetafelpartner2021
 
 #Data inladen 
 df = pd.read_excel('Running Dinner eerste oplossing 2022.xlsx')
@@ -152,7 +182,7 @@ count_hoofdgerecht2022 = hoofdgerecht2022(gt, df_kookte_2021)
 print(count_hoofdgerecht2022)
 count_voorkeur = voorkeursgang(df, df_adressen)
 print(count_voorkeur)
-count_zelfde_tafelpartner_2021 = zelfdetafelpartners2021(df, df_tafelgenoot_2021)
+count_zelfde_tafelpartner_2021 = zelfdetafelpartners2022(df, df_tafelgenoot_2021)
 print(count_zelfde_tafelpartner_2021)
 count_met_buren_aan_tafel = metdeburenaantafel(df, df_buren) 
 print(count_met_buren_aan_tafel)
