277ce9ce715eee571f4bfb384507cc0a63ccfc62
TFHVoe <510735@student.fontys.nl>
2023-09-28 08:50:25 +0200

    ok

--- Josine.py
+++ Josine.py
@@ -2,45 +2,40 @@ import pandas as pd
 #twee verschillende deelnemers zo weining mogelijk met elkaar aan tafel
 df = pd.read_excel('Running Dinner eerste oplossing 2022.xlsx')
 
-#Functie die telt hoe vaak deelnemers in 2023 meerdere malen elkaars tafelgenoten zijn."""
-count_dezelfdetafelgenoot2ofmeer = 0
-lst_2023 = []
-for k in range(3,6):
-    for j in range(len(df)):
-        x = df.iloc[:,1][df.iloc[:,k] == df.iloc[j,k]] #Lijst van alle paren die bij elkaar gaan zitten per gang.
-        for i in x:
-            lst_2023.append((df.iloc[j,1],i))
+def meerdermalentafelgenoot(df):
+    """Functie die telt hoe vaak deelnemers in 2023 meerdere malen elkaars tafelgenoten zijn."""
 
-for i in lst_2023:
-    j = list(i)
-    if j[0] == j[1]:   # Verwijdert het personen paar wanneer de personen in het personenpaar hetzelfde zijn.
-         lst_2023.remove(i)
+    lst_2023 = []
+    for k in range(3,6):
+        for j in range(len(df)):
+            x = df.iloc[:,1][df.iloc[:,k] == df.iloc[j,k]] #Lijst van alle paren die bij elkaar gaan zitten per gang.
+            for i in x:
+                lst_2023.append((df.iloc[j,1],i))
 
-con = []
-lst_2023_2 = []
-for i in lst_2023:
-    if i[0] + i[1] and i[1] + i[0] not in con: #Controleert of het personen paar niet twee keer voor komt in geval dat a = b en b = a.
-        con.append(i[0]+i[1])
-        lst_2023_2.append(i)
+    for i in lst_2023:
+        j = list(i)
+        if j[0] == j[1]:   # Verwijdert het personen paar wanneer de personen in het personenpaar hetzelfde zijn.
+             lst_2023.remove(i)
 
+    sorted_lst_2023 = []
+    for i in lst_2023:  #Sorteerd de inhoud van tuples in de lijst 2023 op alfabetische volgorde.
+        sorted_lst_2023.append(tuple(sorted(i)))
 
+    unique_lst_2023 = []
+    for i in sorted_lst_2023:
+       if i not in unique_lst_2023: #Het vullen van een lijst met de unique tuples uit de lijst list_2023.
+            unique_lst_2023.append(i)
 
-koppels2023 = [] #lijst met alle duo's die elkaar tegenkomen.
-for i in lst_2023_2:
-    koppel = i[0]+i[1]
-    koppels2023.append(koppel)
-    
+    unique_lst_2023_amount = []
+    for i in unique_lst_2023:
+        count_amount = 0        #Het tellen hoe vaak er een tuple in de lijst sorted_lst_2023 zit per unique tuple
+        for j in sorted_lst_2023:
+            if i == j:
+                count_amount += 1
+        unique_lst_2023_amount.append(count_amount)
+    count_dezelfdetafelgenoot2ofmeer = 0
 
-for k in koppels2023:
-    if k[0] == k[1]:
-        count_dezelfdetafelgenoot2ofmeer +=1
-print(count_dezelfdetafelgenoot2ofmeer)
-
-
-#print(koppels2023[1])
-#Data inladen 
-df = pd.read_excel('Running Dinner eerste oplossing 2022.xlsx')
-df_adressen = pd.read_excel("Running Dinner dataset 2022.xlsx",sheet_name="Adressen" )
-df_kookte_2021 = pd.read_excel("Running Dinner dataset 2022.xlsx",sheet_name="Kookte vorig jaar")
-df_tafelgenoot_2021 = pd.read_excel("Running Dinner dataset 2022.xlsx",sheet_name="Tafelgenoot vorig jaar")
-df_buren = pd.read_excel("Running Dinner dataset 2022.xlsx",sheet_name="Buren" )
+    for i in unique_lst_2023_amount:
+        if i >= 2:
+            count_dezelfdetafelgenoot2ofmeer += 1 #Het tellen hoe vaak er een twee personen meer dan twee keer bij elkaar aan aantal aan tafel zitten.
+    return count_dezelfdetafelgenoot2ofmeer
--- poging3.py
+++ poging3.py
@@ -4,6 +4,43 @@ import pandas as pd
 #Functies eisen: 
 
 #Functies wensen:
+def meerdermalentafelgenoot(df):#Functi die telt hoe vaak er twee personen meer dat twee keer aan de zelfde tafel zitten.
+    """Functie die telt hoe vaak deelnemers in 2023 meerdere malen elkaars tafelgenoten zijn."""
+
+    lst_2023 = []
+    for k in range(3,6):
+        for j in range(len(df)):
+            x = df.iloc[:,1][df.iloc[:,k] == df.iloc[j,k]] #Lijst van alle paren die bij elkaar gaan zitten per gang.
+            for i in x:
+                lst_2023.append((df.iloc[j,1],i))
+
+    for i in lst_2023:
+        j = list(i)
+        if j[0] == j[1]:   # Verwijdert het personen paar wanneer de personen in het personenpaar hetzelfde zijn.
+             lst_2023.remove(i)
+
+    sorted_lst_2023 = []
+    for i in lst_2023:  #Sorteerd de inhoud van tuples in de lijst 2023 op alfabetische volgorde.
+        sorted_lst_2023.append(tuple(sorted(i)))
+
+    unique_lst_2023 = []
+    for i in sorted_lst_2023:
+       if i not in unique_lst_2023: #Het vullen van een lijst met de unique tuples uit de lijst list_2023.
+            unique_lst_2023.append(i)
+
+    unique_lst_2023_amount = []
+    for i in unique_lst_2023:
+        count_amount = 0        #Het tellen hoe vaak er een tuple in de lijst sorted_lst_2023 zit per unique tuple
+        for j in sorted_lst_2023:
+            if i == j:
+                count_amount += 1
+        unique_lst_2023_amount.append(count_amount)
+    count_dezelfdetafelgenoot2ofmeer = 0
+
+    for i in unique_lst_2023_amount:
+        if i >= 2:
+            count_dezelfdetafelgenoot2ofmeer += 1 #Het tellen hoe vaak er een twee personen meer dan twee keer bij elkaar aan aantal aan tafel zitten.
+    return count_dezelfdetafelgenoot2ofmeer
 def hoofdgerecht2022(gt, df_kookte_2021): #Functie die telt hoe vaak een huis houden het hoofdgerecht vorige jaar en dit jaar moet koken.
     """Functie die telt hoe vaak een huishouden dat in 2022 een hoofdgerecht bereid heeft, ook een hoofdgerecht bereidt tijdens de komende Running Dinner."""
     count_hoofdgerecht2022 = 0 #Count van aantal.
@@ -43,8 +80,7 @@ def zelfdetafelpartners2021(df, df_tafelgenoot_2021):#Functie die telt hoevaak t
                 lst_2023.append((df.iloc[j,1],i))
 
        for i in lst_2023:
-           j = list(i)
-           if j[0] == j[1]:   # Verwijdert het personen paar wanneer de personen in het personenpaar hetzelfde zijn.
+           if i[0] == i[1]:   # Verwijdert het personen paar wanneer de personen in het personenpaar hetzelfde zijn.
                lst_2023.remove(i)
 
     con = []
@@ -120,3 +156,5 @@ count_zelfde_tafelpartner_2021 = zelfdetafelpartners2021(df, df_tafelgenoot_2021
 print(count_zelfde_tafelpartner_2021)
 count_met_buren_aan_tafel = metdeburenaantafel(df, df_buren) 
 print(count_met_buren_aan_tafel)
+count_meer_dan_twee_keer_zelfde_persoon = meerdermalentafelgenoot(df)
+print(count_meer_dan_twee_keer_zelfde_persoon)
