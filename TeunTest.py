ad636de2ca2a8e4bce8548abf40f553b449b7a98
TFHVoe <510735@student.fontys.nl>
2023-09-28 16:57:40 +0200

    pushee

--- TeunTest.py
+++ TeunTest.py
@@ -1,30 +1,32 @@
-def zelfdetafelpartners2021(df, df_tafelgenoot_2021):#Functie die telt hoevaak twee tafelgenoten in 2022 ook de tafelgenoot van 2023 waren.
-    """Functie die telt hoe vaak twee deelnemers die in 2022 bij elkaar aan tafel zaten, in 2023 weer elkaars tafelgenoot waren."""
-    countzelfdetafelpartner2021 = 0
-    lst = []
-    for i in range(1,len(df_tafelgenoot_2021)):
-        lst.append((df_tafelgenoot_2021.iloc[i,0], df_tafelgenoot_2021.iloc[i,1])) #Lijst met alle paren die bij elkaar aan tafel hebben gezeten in 2021.
+import pandas as pd
 
-    lst_2023 = []
-    for k in range(3,6):
-       for j in range(len(df)):
-           x = df.iloc[:,1][df.iloc[:,k] == df.iloc[j,k]] #Lijst van alle paren die bij elkaar gaan zitten per gang.
-           for i in x:
-                lst_2023.append((df.iloc[j,1],i))
+df = pd.read_excel('Running Dinner eerste oplossing 2022.xlsx')
 
-       for i in lst_2023:
-           if i[0] == i[1]:   # Verwijdert het personen paar wanneer de personen in het personenpaar hetzelfde zijn.
-               lst_2023.remove(i)
+ts = {} #Tafelschikking D X G --> A
+gangen = ['Voor','Hoofd','Na']
+for j in gangen:
+    for i in range(len(df)):
+        ts[(df.iloc[i, 1], j)] = df.loc[i, j]
+#print(ts)
 
-    con = []
-    lst_2023_2 = []
-    for i in lst_2023:
-        if i[0] + i[1] and i[1] + i[0] not in con: #Controleert of het personen paar niet twee keer voor komt in geval dat a = b en b = a.
-            con.append(i[0]+i[1])
-            lst_2023_2.append(i)
+lst_unique = []
+for i, j in ts.items():
+    for k in i[0:2:len(i)]:
+        if k not in lst_unique:
+            lst_unique.append(k)
 
-    for i in lst_2023_2:
-        for j in lst:
-            if i[0]+i[1] == j[0]+j[1] or i[1]+i[0] == j[0]+j[1] or i[0]+i[1] == j[1]+j[0] or i[1]+i[0] == j[1]+j[0]: #Telt 1 op bij de teller wanneer twee personen vorig jaar en dit jaar bij elkaar aan tafel zaten. 
-                countzelfdetafelpartner2021 += 1
-    return countzelfdetafelpartner2021
+lst_amount = []
+gangen = ['Voor','Hoofd','Na']
+for i in lst_unique:
+    countabc = 0
+    for j, k in ts.items():
+        for l in gangen:
+            if i == j[0] and j[1] == l:
+                countabc += 1
+    lst_amount.append(countabc)
+
+for i in lst_amount:
+    if i != 3:
+        print('Voldoet niet')
+print(lst_amount)
+
