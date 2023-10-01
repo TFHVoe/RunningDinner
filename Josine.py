85600ea7a7b158f02a61530513e2261db31ffdee
Josinev <509931@student.fontys.nl>
2023-10-01 21:14:36 +0200

    eis2J

--- Josine.py
+++ Josine.py
@@ -78,3 +78,12 @@ for i in range(len(df)):
     
     
 # Een heel klein aantal groepjes van deelnemers, vaak één of twee duo’s, zit tijdens het gehele Running Dinner voor elke gang bij elkaar aan tafel
+setl = df.loc[df['Bewoner'] == "WO_59_V_Els"] 
+set1a = df.loc[df['Bewoner'] == "WO_59_M_Dré"]
+set1a.reset_index(drop = True, inplace = True)
+setl.reset_index(drop = True, inplace = True) 
+if setl["Voor"][0] == set1a["Voor"][0] and setl["Hoofd"][0] == set1a["Hoofd"][0] and setl["Na"][0] ==set1a["Na"][0]:
+    print("jaaaaaaaaaaaa")
+    
+    
+
