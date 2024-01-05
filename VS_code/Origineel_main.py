6551d4ecc9985838427b46a92a1929bcc1100555
TFHVoe <t.voesenek@student.fontys.nl>
2024-01-05 13:13:58 +0100

    Nieuwe visualisaties

--- VS_code/Origineel_main.py
+++ VS_code/Origineel_main.py
@@ -12,7 +12,7 @@ handlers=[logging.FileHandler("sa.log"),logging.StreamHandler(stream=sys.stdout)
 logging.getLogger('matplotlib.font_manager').disabled = True
  
 #Functies eisen: 
-def ElkeGangAnderAdres(ts):#Functie die telt hoe vaak er niet door een persoon een voor, hoofd en nagerecht gegeten wordt en dat dit op een ander adres is.
+def elkeganganderadres(ts):#Functie die telt hoe vaak er niet door een persoon een voor, hoofd en nagerecht gegeten wordt en dat dit op een ander adres is.
     """Functie die telt hoe vaak niet door ieder persoon 3 gangen op verschillende adressen gegeten wordt."""
     lst_unique = []
     for i, j in ts.items():
@@ -37,7 +37,7 @@ def ElkeGangAnderAdres(ts):#Functie die telt hoe vaak er niet door een persoon e
         if i != 3:      #Een for loop die telt hoe vaak er niet voldaan wordt aan de eisen dat er een voor, hoofd en nagerecht gegeten wordt en dat dit op een ander adres is.    
             fout_count += 1
     return fout_count
-def MoetKoken(df):#Functie die telt hoe vaak er een persoon niet kookt die wel moet koken.    """Functie die telt hoe vaak het kook adres niet gelijk is aan het huisadres."""
+def moetkoken(df):#Functie die telt hoe vaak er een persoon niet kookt die wel moet koken.    """Functie die telt hoe vaak het kook adres niet gelijk is aan het huisadres."""
     """Functie die telt hoeveel mensen die een van de gangen moet koken niet kookt."""
     count_kook_adres_is_niet_huisadres = 0
     gangen = ['Voor','Hoofd','Na']
@@ -48,7 +48,7 @@ def MoetKoken(df):#Functie die telt hoe vaak er een persoon niet kookt die wel m
     return count_kook_adres_is_niet_huisadres
     
     return count_bewoners_die_moeten_koeken_maar_niet_koken         
-def KookadresIsHuisadres(df):#Functie die telt hoe vaak het kook adres niet gelijk is aan het thuisaders.
+def kookadresishuisadres(df):#Functie die telt hoe vaak het kook adres niet gelijk is aan het thuisaders.
     """Functie die telt hoe vaak het kook adres niet gelijk is aan het huisadres."""
     count_kook_adres_is_niet_huisadres = 0
     gangen = ['Voor','Hoofd','Na']
@@ -57,7 +57,7 @@ def KookadresIsHuisadres(df):#Functie die telt hoe vaak het kook adres niet geli
             if df.iloc[i,2] != df.loc[i,df.iloc[i,6]]: #Controleert of het huisadres gelijk is aan het kookaders.
                 count_kook_adres_is_niet_huisadres +=1 
     return count_kook_adres_is_niet_huisadres 
-def CountAantalEtersVoldoed(df, df_adressen):#Functie die telt hoe vaak het gasten aantal waarvoor ze moeten koken buit het gasten aantal waarvoor ze kunnen koken ligt.
+def countaantaletersvoldoed(df, df_adressen):#Functie die telt hoe vaak het gasten aantal waarvoor ze moeten koken buit het gasten aantal waarvoor ze kunnen koken ligt.
     """Functie die telt hoevaak een bewoner voor een gaste aantal moet koken dat buiten het minimun of maximum valt van gasten waarvoor ze kunnen koken."""
     
     count_aantal_eters_voldoed = 0
@@ -70,30 +70,30 @@ def CountAantalEtersVoldoed(df, df_adressen):#Functie die telt hoe vaak het gast
                     if (df_adressen.iloc[j, 1] <= df.iloc[i,7] <= df_adressen.iloc[j,2]) == False:
                         count_aantal_eters_voldoed += 1        
     return count_aantal_eters_voldoed
-def PaarBijElkaar(df, df_paar_blijft_bij_elkaar):#Functie die telt hoe vaak een paar dat bij elkaar moet blijven niet bij elkaar is.
+def paarbijelkaar(df, df_paar_blijft_bij_elkaar):#Functie die telt hoe vaak een paar dat bij elkaar moet blijven niet bij elkaar is.
     """Functie die telt hoe vaak een paar die bij elkaar moet zitten niet bij elkaar zit."""
 
-    count_PaarNietBijElkaar = 0
+    count_paarnietbijelkaar = 0
     for i in range(1,len(df_paar_blijft_bij_elkaar)):
         a = df.loc[df["Bewoner"]== df_paar_blijft_bij_elkaar.iloc[i,0]]
         b = df.loc[df["Bewoner"]== df_paar_blijft_bij_elkaar.iloc[i,1]]
         a.reset_index(drop = True, inplace = True)
         b.reset_index(drop = True, inplace = True) 
         if (a["Voor"][0] == b["Voor"][0] and a["Hoofd"][0] == b["Hoofd"][0] and b["Na"][0] == a["Na"][0]) == 0:
-            count_PaarNietBijElkaar += 1   
-    return count_PaarNietBijElkaar
+            count_paarnietbijelkaar += 1   
+    return count_paarnietbijelkaar
 
 def eisen(ts, df, df_adressen, df_paar_blijft_bij_elkaar):#Een Functie die alle eisen controleert.
-    count_elke_gang_anders = ElkeGangAnderAdres(ts)
-    moet_koken = MoetKoken(df)
-    kookadres_is_huisadres = KookadresIsHuisadres(df)
-    count_aantal_eters_voldoed = CountAantalEtersVoldoed(df, df_adressen)
-    paar_bij_elkaar = PaarBijElkaar(df, df_paar_blijft_bij_elkaar)
+    count_elke_gang_anders = elkeganganderadres(ts)
+    moet_koken = moetkoken(df)
+    kookadres_is_huisadres = kookadresishuisadres(df)
+    count_aantal_eters_voldoed = countaantaletersvoldoed(df, df_adressen)
+    paar_bij_elkaar = paarbijelkaar(df, df_paar_blijft_bij_elkaar)
 
     return count_elke_gang_anders + moet_koken + kookadres_is_huisadres + count_aantal_eters_voldoed + paar_bij_elkaar
 
 #Functies wensen:
-def MeerdereMalenTafelgenoot(df):#Functie die telt hoe vaak er twee personen meer dat twee keer aan de zelfde tafel zitten.
+def meerdermalentafelgenoot(df):#Functie die telt hoe vaak er twee personen meer dat twee keer aan de zelfde tafel zitten.
     """Functie die telt hoe vaak deelnemers in 2023 meerdere malen elkaars tafelgenoten zijn."""
 
     lst_2023 = []
@@ -124,15 +124,15 @@ def MeerdereMalenTafelgenoot(df):#Functie die telt hoe vaak er twee personen mee
             if i == j:
                 count_amount += 1
         unique_lst_2023_amount.append(count_amount)
-    count_DezelfdeTafelgenoot2OfMeer = 0
+    count_dezelfdetafelgenoot2ofmeer = 0
 
     for i in unique_lst_2023_amount:
         if i > 2:
-            count_DezelfdeTafelgenoot2OfMeer += 1 #Het tellen hoe vaak er een twee personen meer dan twee keer bij elkaar aan aantal aan tafel zitten.
-    return count_DezelfdeTafelgenoot2OfMeer
-def Hoofdgerecht2022(gt, df_kookte_2021): #Functie die telt hoe vaak een huis houden het hoofdgerecht vorige jaar en dit jaar moet koken.
+            count_dezelfdetafelgenoot2ofmeer += 1 #Het tellen hoe vaak er een twee personen meer dan twee keer bij elkaar aan aantal aan tafel zitten.
+    return count_dezelfdetafelgenoot2ofmeer
+def hoofdgerecht2022(gt, df_kookte_2021): #Functie die telt hoe vaak een huis houden het hoofdgerecht vorige jaar en dit jaar moet koken.
     """Functie die telt hoe vaak een huishouden dat in 2022 een hoofdgerecht bereid heeft, ook een hoofdgerecht bereidt tijdens de komende Running Dinner."""
-    count_Hoofdgerecht2022 = 0 #Count van aantal.
+    count_hoofdgerecht2022 = 0 #Count van aantal.
     hh = {} #Huisadress kookte hoofdgerecht vorig jaar.
     for i in range(len(df_kookte_2021)):
         hh[df_kookte_2021.iloc[i,0]] = df_kookte_2021.iloc[i,1]
@@ -140,9 +140,9 @@ def Hoofdgerecht2022(gt, df_kookte_2021): #Functie die telt hoe vaak een huis ho
     for j, k in gt.items():
         for l, m in hh.items():
             if j == l and k == m == 'Hoofd': #Twee for loops die kijken of de huisadressen gelijk zijn en dat het een hoofdgerecht is.
-             count_Hoofdgerecht2022 += 1
-    return count_Hoofdgerecht2022    
-def Voorkeursgang(df, df_adressen):#Functie die telt hoevaak een voorkeur gang juist is.
+             count_hoofdgerecht2022 += 1
+    return count_hoofdgerecht2022    
+def voorkeursgang(df, df_adressen):#Functie die telt hoevaak een voorkeur gang juist is.
     """Functie kijkt of de voorkeur gang van een huisaderes aan dit huisaderes toegekent is."""
     count_voorkeur = 0 #Count van aantal
     vg = {} #Huisadressen met hun voorkeur
@@ -154,9 +154,9 @@ def Voorkeursgang(df, df_adressen):#Functie die telt hoevaak een voorkeur gang j
             if j == l and k == m: #Twee for loops die kijken voor ieder huisaderes of hier de voorkeur gang gekookt wordt.
                 count_voorkeur -= 1
     return count_voorkeur
-def DezelfdeTafelpartners2022(df, df_tafelgenoot_2022):#Functie die telt hoevaak twee tafelgenoten in 2022 ook de tafelgenoot van 2023 waren.
+def zelfdetafelpartners2022(df, df_tafelgenoot_2022):#Functie die telt hoevaak twee tafelgenoten in 2022 ook de tafelgenoot van 2023 waren.
     """Functie die telt hoe vaak twee deelnemers die in 2022 bij elkaar aan tafel zaten, in 2023 weer elkaars tafelgenoot waren."""
-    CountDezelfdeTafelpartner2022 = 0
+    countzelfdetafelpartner2022 = 0
     lst = []
     for i in range(1,len(df_tafelgenoot_2022)):
         lst.append((df_tafelgenoot_2022.iloc[i,0], df_tafelgenoot_2022.iloc[i,1])) #Lijst met alle paren die bij elkaar aan tafel hebben gezeten in 2022.
@@ -182,12 +182,12 @@ def DezelfdeTafelpartners2022(df, df_tafelgenoot_2022):#Functie die telt hoevaak
     for i in lst_2023_2:
         for j in lst:
             if i[0]+i[1] == j[0]+j[1] or i[1]+i[0] == j[0]+j[1] or i[0]+i[1] == j[1]+j[0] or i[1]+i[0] == j[1]+j[0]: #Telt 1 op bij de teller wanneer twee personen vorig jaar en dit jaar bij elkaar aan tafel zaten. 
-                CountDezelfdeTafelpartner2022 += 1
+                countzelfdetafelpartner2022 += 1
 
-    return CountDezelfdeTafelpartner2022
-def MetDeBurenAanTafel(df, df_buren):#Functie die telt hoevaak er met de driecte buren aan tafel gezeten wordt.
+    return countzelfdetafelpartner2022
+def metdeburenaantafel(df, df_buren):#Functie die telt hoevaak er met de driecte buren aan tafel gezeten wordt.
     """Functie die telt hoe vaak er met de directe buren aan tafel gezeten wordt"""
-    CountDezelfdeBuren = 0
+    countzelfdeburen = 0
     lst = []
     for i in range(1,len(df_buren)):
         lst.append((df_buren.iloc[i,0], df_buren.iloc[i,1])) #Lijst met alle buren.
@@ -214,11 +214,11 @@ def MetDeBurenAanTafel(df, df_buren):#Functie die telt hoevaak er met de driecte
     for i in lst_2023_2:
             for j in lst:
                 if i[0]+i[1] == j[0]+j[1] or i[1]+i[0] == j[0]+j[1] or i[0]+i[1] == j[1]+j[0] or i[1]+i[0] == j[1]+j[0]: #Telt 1 op wanneer er met de directe buren aan tafel gezeten wordt.
-                    CountDezelfdeBuren += 1
-    return CountDezelfdeBuren
-def DezelfdeTafelpartners2021(df, df_tafelgenoot_2021):#Functie die telt hoevaak twee tafelgenoten in 2021 ook de tafelgenoot van 2023 waren.
+                    countzelfdeburen += 1
+    return countzelfdeburen
+def zelfdetafelpartners2021(df, df_tafelgenoot_2021):#Functie die telt hoevaak twee tafelgenoten in 2021 ook de tafelgenoot van 2023 waren. 
     """Functie die telt hoe vaak twee deelnemers die in 2022 bij elkaar aan tafel zaten, in 2023 weer elkaars tafelgenoot waren."""
-    CountDezelfdeTafelpartner2021 = 0
+    countzelfdetafelpartner2021 = 0
     lst = []
     for i in range(1,len(df_tafelgenoot_2021)):
         lst.append((df_tafelgenoot_2021.iloc[i,0], df_tafelgenoot_2021.iloc[i,1])) #Lijst met alle paren die bij elkaar aan tafel hebben gezeten in 2021.
@@ -244,16 +244,16 @@ def DezelfdeTafelpartners2021(df, df_tafelgenoot_2021):#Functie die telt hoevaak
     for i in lst_2023_2:
         for j in lst:
             if i[0]+i[1] == j[0]+j[1] or i[1]+i[0] == j[0]+j[1] or i[0]+i[1] == j[1]+j[0] or i[1]+i[0] == j[1]+j[0]: #Telt 1 op bij de teller wanneer twee personen vorig jaar en dit jaar bij elkaar aan tafel zaten. 
-                CountDezelfdeTafelpartner2021 += 1
-    return CountDezelfdeTafelpartner2021
+                countzelfdetafelpartner2021 += 1
+    return countzelfdetafelpartner2021
 
 def wensen(df, gt, df_kookte_2022, df_adressen, df_tafelgenoot_2022, df_buren, df_tafelgenoot_2021):#Een Functie die alle wensen controleert.
-    count_meer_dan_twee_keer_zelfde_persoon = MeerdereMalenTafelgenoot(df)
-    count_hoofdgerecht2022 = Hoofdgerecht2022(gt, df_kookte_2022)
-    count_voorkeur = Voorkeursgang(df, df_adressen)
-    count_zelfde_tafelpartner_2022 = DezelfdeTafelpartners2022(df, df_tafelgenoot_2022)
-    count_met_buren_aan_tafel = MetDeBurenAanTafel(df, df_buren)
-    count_zelfde_tafelpartner_2021 = DezelfdeTafelpartners2021(df, df_tafelgenoot_2021)
+    count_meer_dan_twee_keer_zelfde_persoon = meerdermalentafelgenoot(df)
+    count_hoofdgerecht2022 = hoofdgerecht2022(gt, df_kookte_2022)
+    count_voorkeur = voorkeursgang(df, df_adressen)
+    count_zelfde_tafelpartner_2022 = zelfdetafelpartners2022(df, df_tafelgenoot_2022)
+    count_met_buren_aan_tafel = metdeburenaantafel(df, df_buren)
+    count_zelfde_tafelpartner_2021 = zelfdetafelpartners2021(df, df_tafelgenoot_2021)
 
     return  count_meer_dan_twee_keer_zelfde_persoon * 0.3 + count_hoofdgerecht2022 * 0.2 + count_voorkeur * 0.175 + count_zelfde_tafelpartner_2022 * 0.15 + count_met_buren_aan_tafel * 0.125 + count_zelfde_tafelpartner_2021 * 0.05
 
@@ -281,14 +281,12 @@ for j in gangen:
 #2 opt toegepast bij running dinner
 moment0 = wensen(df, gt, df_kookte_2022, df_adressen, df_tafelgenoot_2022, df_buren, df_tafelgenoot_2021)
 logger.debug(msg=f'Start:{moment0}')
-class ItteratiePerGang(Exception): pass
-class MaxItteratie(Exception):pass
+class itteratiepergang(Exception): pass
 
-def plot(X, Y, Y1):#De plot van de iteraties tegen de oplossing voor deze iteratie
+def plot(X, Y):#De plot van de iteraties tegen de oplossing voor deze iteratie
     """Functie voor het plotten van de grafiek: iteraties tegen de oplossing voor deze iteratie """
     plt.plot(X,Y)
-    plt.plot(X,Y1, 'r')
-    plt.title('Uitkomsten Toegestanen Oplossingen per Iteratie')
+    plt.title('Uitkomsten Toegestanen Oplossingen per Itteratie')
     plt.xlabel('Itteratie')
     plt.ylabel('Oplossing')
     plt.show()
@@ -299,10 +297,8 @@ improved = True
 gangen = ['','','Voor','Hoofd','Na']
 X = []
 Y = []
-Y1 = []
 X.append(0)
 Y.append(moment0)
-Y1.append(moment0)
 while improved:
     impoved = False
     for k in range(2,5):
@@ -343,25 +339,20 @@ while improved:
 
                                 X.append(itteratie)
                                 Y.append(sol)
-                                
+
+                                if itteratie % 1000 == 0:
+                                    df.to_excel('Verbeterde planning Running dinner 2023.xlsx', index = False) #Als er 1000 itearteis zijn geweest wordt er een grafiek gemaakt en een oplossing in een excel bestand gegenereerd.
+                                    plot(X,Y)
+
 
                                 if sol < start: #Wanneer de oplossing kleiner is dan de start wordt de oplossing de nieuwe start.
                                     df = df_new
                                     improved = True
                                     logger.debug(msg=f'Oplossing:{sol}')
-                                    Y1.append(sol)
-                                else:
-                                    Y1.append(start)
-
-                                if itteratie % 1000 == 0:
-                                    df.to_excel('Verbeterde planning Running dinner 2023.xlsx', index = False) #Als er 1000 itearteis zijn geweest wordt er een grafiek gemaakt en een oplossing in een excel bestand gegenereerd.
-                                    plot(X,Y,Y1)
-
-                                    
+                            
                                 if loc_itteratie == 3:
-                                    raise ItteratiePerGang()
-                                
-                                    
-                
-        except ItteratiePerGang:
+                                    raise itteratiepergang()
+
+               
+        except itteratiepergang:
             pass
